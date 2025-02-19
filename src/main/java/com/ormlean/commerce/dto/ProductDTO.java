e567bc817419e8e42265293f0e49d22b14771dee
mmont <mmont@Maycon>
2025-02-19 12:39:23 -0300

    find mapped list and implement modelmapper

--- src/main/java/com/ormlean/commerce/dto/ProductDTO.java
+++ src/main/java/com/ormlean/commerce/dto/ProductDTO.java
@@ -2,8 +2,6 @@ package com.ormlean.commerce.dto;
 
 import java.math.BigDecimal;
 
-import com.ormlean.commerce.entities.Product;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,13 +16,5 @@ public class ProductDTO {
 	private String description;
 	private BigDecimal price;
 	private String imgUrl;
-
-	public ProductDTO(Product product) {
-		id = product.getId();
-		name = product.getName();
-		description = product.getDescription();
-		price = product.getPrice();
-		imgUrl = product.getImgUrl();
-	}
 
 }
--- src/main/java/com/ormlean/commerce/services/ProductService.java
+++ src/main/java/com/ormlean/commerce/services/ProductService.java
@@ -1,10 +1,16 @@
 package com.ormlean.commerce.services;
 
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ormlean.commerce.dto.ProductDTO;
+import com.ormlean.commerce.entities.Product;
 import com.ormlean.commerce.repositories.ProductRepository;
 
 @Service
@@ -12,10 +18,24 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private ModelMapper mapper;
 
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
-		return new ProductDTO(productRepository.findById(id).get());
+		Optional<Product> product = productRepository.findById(id);
+
+		return mapper.map(product.get(), ProductDTO.class);
 	}
 
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> products = productRepository.findAll(pageable);
+
+		return convertToDtoList(products);
+	}
+
+	public Page<ProductDTO> convertToDtoList(Page<Product> products) {
+		return products.map(p -> mapper.map(p, ProductDTO.class));
+	}
 }
